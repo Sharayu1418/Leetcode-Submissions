f93dff274d82b6068864dd0f7f1b7353820da9ac
Sharayu1418 <srr10019@nyu.edu>
2025-03-31 15:54:37 -0400

    Create implement_queue-using-stacks.py

---
+++ 8023-459-232-implement-queue-using-stacks/implement_queue-using-stacks.py
@@ -0,0 +1,34 @@
+class MyQueue:
+
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def push(self, x: int) -> None:
+        self.stack1.append(x)
+
+    def pop(self) -> int:
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2.pop()
+
+    def peek(self) -> int:
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2[-1]
+
+    def empty(self) -> bool:
+        return not self.stack1 and not self.stack2
+
+obj = MyQueue()
+obj.push(1)
+obj.push(2)
+param_3 = obj.peek()
+param_2 = obj.pop()
+param_4 = obj.empty()
+
+print(param_3)
+print(param_2)
+print(param_4)
