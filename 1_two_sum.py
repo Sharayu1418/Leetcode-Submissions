51b01915f5dc3e9d1fb22efa50d055b8746546ce
Sharayu1418 <srr10019@nyu.edu>
2025-11-17 13:41:53 -0500

    two pointer sol

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -1,9 +1,38 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums, target):
+        # Create (value, index) pairs
+        #paired = [(value, index) for index, value in enumerate(nums)]
+        paired = []
         for i in range(len(nums)):
-         for j in range (i+1, len(nums)):
-             if  nums[i] + nums[j] == target:
-                 return [i,j]
+            paired.append((nums[i], i))
+
+        # Sort by the values
+        paired.sort()
+
+        left = 0
+        right = len(paired) - 1
+
+        # Two-pointer scan
+        while left < right:
+            left_val, left_idx = paired[left]
+            right_val, right_idx = paired[right]
+
+            total = left_val + right_val
+
+            if total == target:
+                return [left_idx, right_idx]
+
+            elif total < target:
+                left += 1
+            else:
+                right -= 1
+
+#class Solution:
+    #def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #for i in range(len(nums)):
+         #for j in range (i+1, len(nums)):
+             #if  nums[i] + nums[j] == target:
+                 #return [i,j]
 
     #def twoSum(self, nums, target):
         #notebook = {}   # hash map
